22088ff1c1b5a6d35a9c68bd9baaad75f9a90057
mehdi <dehghani_m@rocketmail.com>
2023-02-11 11:30:08 +0330

    make data class

---
+++ app/src/main/java/com/example/sourcebbcnews/models/Article.kt
@@ -0,0 +1,15 @@
+package com.example.sourcebbcnews.models
+
+
+
+data class Article(
+    var id : Int? = null,
+    val author : String?,
+    val content : String?,
+    val description : String?,
+    val publishedAt : String?,
+    val source : Source?,
+    val title : String?,
+    val url : String?,
+    val urlToImage : String?
+)
