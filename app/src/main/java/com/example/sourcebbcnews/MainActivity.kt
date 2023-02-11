f17ff2099796d95faf008b21a731a34736e5ccd6
mehdi <dehghani_m@rocketmail.com>
2023-02-11 11:18:42 +0330

    use binding

--- app/src/main/java/com/example/sourcebbcnews/MainActivity.kt
+++ app/src/main/java/com/example/sourcebbcnews/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.sourcebbcnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sourcebbcnews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
