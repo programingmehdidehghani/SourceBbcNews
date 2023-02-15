ba19d3d17b759916f967f3900ffabd7477d40202
mehdi <dehghani_m@rocketmail.com>
2023-02-15 22:02:05 +0330

    make content screen

---
+++ app/src/main/java/com/example/sourcebbcnews/ui/NewsContentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sourcebbcnews.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sourcebbcnews.R
+
+class NewsContentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_content)
+    }
+}
