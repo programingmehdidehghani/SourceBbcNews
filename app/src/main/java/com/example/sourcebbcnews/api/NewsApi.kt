41322d7828ccbbc165c728df3d2c5704ad08df91
mehdi <dehghani_m@rocketmail.com>
2023-02-15 14:41:00 +0330

    continue make project

--- app/src/main/java/com/example/sourcebbcnews/api/NewsApi.kt
+++ app/src/main/java/com/example/sourcebbcnews/api/NewsApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 
 interface NewsApi {
 
-    @GET("v2/top-headlines")
+    @GET("/v2/top-headlines")
     suspend fun getBreakingNews(
         @Query("country")
         countryCode : String = "us",
--- app/src/main/java/com/example/sourcebbcnews/ui/MainActivity.kt
+++ app/src/main/java/com/example/sourcebbcnews/ui/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.sourcebbcnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.example.sourcebbcnews.databinding.ActivityMainBinding
+import com.example.sourcebbcnews.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,5 +20,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        newsViewModel.breakingNews.observe(this , Observer { response ->
+            when(response){
+                is Resource.Success -> {
+                    response.data?.let { newsResponse ->
+                      Log.i("tag","data is ..."+response)
+                    }
+                }
+                is Resource.Error -> {
+                    response.message?.let { message ->
+                        Toast.makeText(this, "An error occured: $message", Toast.LENGTH_LONG).show()
+                    }
+                }
+                is Resource.Loading -> {
+
+                }
+            }
+        })
     }
 }
